4c77b89e7d6eec8c2d06b3af3c60ce3bae4932b2
trisula-digiart <kanzalistianm@gmail.com>
2026-07-27 13:59:55 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,7 +5,7 @@
 
 const CONFIG = {
   // Ganti URL di bawah dengan URL Deployment Google Apps Script Web App Anda
-  GAS_API_URL: "https://script.google.com/macros/s/AKfycbxYOUR_DEPLOYED_GAS_WEB_APP_ID/exec",
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbwNBkqK8Z9M5JctiwSsvaxoQKEt8sm6tvw6ps2S1gShf2WTDm10Ym-t4qEE3Z11n85f/exec",
   
   // Passcode Rahasia Internal Tim WO untuk Dashboard Admin
   ADMIN_PASSCODE: "WO-ADMIN-2026",
